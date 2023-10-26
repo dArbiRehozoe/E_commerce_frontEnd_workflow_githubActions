8836ef84bad40be67cec67fb6e7cdec166dca620
dArbiRehozoe <rehozoedarbi@gmail.com>
2023-10-26 15:04:11 -0700

    firt piplene

--- changetag.py
+++ changetag.py
@@ -1,6 +1,6 @@
 import requests
 import os
-TAG_TO_REPLACE = os.environ.get("TAG_TO_REPLACE","0.42")
+TAG_TO_REPLACE = os.environ.get("TAG_TO_REPLACE","0.1")
 DOCKERHUB_USERNAME = os.environ.get("DOCKERHUB_USERNAME")
 DOCKERHUB_REPO = os.environ.get("DOCKERHUB_REPO","testpush")
 DOCKERHUB_TOKEN = os.environ.get("DOCKERHUB_TOKEN")
